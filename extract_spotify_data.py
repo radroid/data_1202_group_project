5ffb26f0b26afb057e0a8ef10bf050b98a5bab6e
Raj Dholakia <raj9dholakia@gmail.com>
2021-01-29 07:34:53 +0530

    Update code - Add code to extract songs from Spotify's charts data. - Update .gitignore

---
+++ extract_spotify_data.py
@@ -0,0 +1,14 @@
+from fycharts.SpotifyCharts import SpotifyCharts
+from pathlib import Path
+import logging
+import datetime
+
+
+filepath = Path("global_top_200_daily.csv")
+regions = ["global"]
+
+if not filepath.exists():
+    api = SpotifyCharts()
+    api.top200Daily(output_file = str(filepath), region = regions)
+else:
+    logging.debug(f"{datetime.datetime.now().replace(microsecond=0)}: No data downloaded.")
