8a37df10715124684004fab9c6549331f9d8c9bd
Raj Dholakia <raj9dholakia@gmail.com>
2021-01-29 17:13:36 +0530

    Update - .gitignore with jupyter notebook cache files - Add jupyter notebook - extract top 100 songs of all regions from 2017.

--- extract_spotify_data.py
+++ extract_spotify_data.py
@@ -5,11 +5,11 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-filepath = Path("global_top_200_daily.csv")
-regions = ["global"]
+filepath = Path("all_regions_top_200_daily.csv")
+# regions = ["global"]
 
 if not filepath.exists():
     api = SpotifyCharts()
-    api.top200Daily(output_file = str(filepath), region = regions)
+    api.top200Daily(output_file = str(filepath))
 else:
     logging.debug(f"{datetime.datetime.now().replace(microsecond=0)}: No data downloaded.")
